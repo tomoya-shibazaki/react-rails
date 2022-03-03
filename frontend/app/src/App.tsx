734c72a758a42560871ce43fe5fd03b06e935190
tomoya shibazaki <tshibazaki8165@gmail.com>
2022-03-04 00:31:24 +0900

    frontend add create

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -4,9 +4,10 @@ import './App.css';
 import axios from "axios"
 
 
-
 const App = () => {
   const [users, setUsers] = useState([])
+  const [values, setValues] = useState({name: '', email: ''})
+
 
   const fetchUsers = async () => {
     const response = await axios({
@@ -18,10 +19,33 @@ const App = () => {
     setUsers(response.data.data)
   }
 
+  const createUser = async () => {
+    const response = await axios({
+      method: 'post',
+      url: `http://localhost:3001/api/v1/users`,
+      withCredentials: false,
+      headers: {'Access-Control-Allow-Origin' : '*'},
+      data: values
+    })
+    fetchUsers()
+  }
+
+  const doChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value
+    const key = e.target.name
+    setValues({...values, [key]: newValue})
+  }
+  const doSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const data = JSON.stringify(values)
+    window.alert(data)
+    createUser()
+  }
+
   useEffect (() => {
     fetchUsers()
   }, [])
-  console.log(users)
+
   const renderList = users.map((user) => {
     const { id, name, email, created_at, updated_at } = user
 
@@ -29,13 +53,35 @@ const App = () => {
       <tr key={id}>
         <td data-label="id">{id}</td>
         <td data-label="name">{name}</td>
-        <td data-label="email">$ {email}</td>
+        <td data-label="email">{email}</td>
         <td data-label="created_at">{created_at}</td>
         <td data-label="updated_at">{updated_at}</td>
       </tr>
     )
     })
     return(
+      <>
+        <div>
+          <form onSubmit={doSubmit}>
+            <label>
+              name:
+              <input 
+                type="text" 
+                name="name"
+                value={values.name}
+                onChange={doChange}
+              />
+              email:
+              <input 
+                type="text"
+                name="email" 
+                value={values.email}
+                onChange={doChange}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
         <div>
           <table className='ui celled table'>
             <thead>
@@ -51,7 +97,8 @@ const App = () => {
             <>{renderList}</>
           </tbody>
         </table>
-      </div>
+       </div>
+      </>
     )
 }
 
